9c27f5f12e6768aac75467e39990dea9539aaa94
pallavieee <pallaviisinghh2222@gmail.com>
2022-12-31 19:48:26 +0530

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var numberOfDrumButtons= document.querySelectorAll(".drum").length;
 
 for(var i=0; i<numberOfDrumButtons;i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){ //anonymous function
         alert("I got clicked");
-    });
+    }); // event listener takes 2 inputs 1 parameter specifies what event it should listen to 2 parameter specief what it should do after listening to event.
 }
